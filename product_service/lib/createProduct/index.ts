7f5a9660289c7659ea5283673db80a4116cf4047
KazikKluz <kazik.kluz@gmail.com>
2025-03-02 02:32:35 +0000

    fix: loggs format

--- product_service/lib/createProduct/index.ts
+++ product_service/lib/createProduct/index.ts
@@ -3,5 +3,11 @@ import createProduct from './controller';
 exports.handler = async (
   event: APIGatewayEvent
 ): Promise<APIGatewayResponse> => {
+  console.log('createProduct request', {
+    path: event.path,
+    method: event.httpMethod,
+    parameters: event.pathParameters,
+    body: event.body,
+  });
   return await createProduct(event.body);
 };
--- product_service/lib/getProductsList/index.ts
+++ product_service/lib/getProductsList/index.ts
@@ -3,5 +3,11 @@ import getProductsList from './controller';
 exports.handler = async (
   event: APIGatewayEvent
 ): Promise<APIGatewayResponse> => {
+  console.log('getProductsList request', {
+    path: event.path,
+    method: event.httpMethod,
+    parameters: event.pathParameters,
+    body: event.body,
+  });
   return await getProductsList();
 };
