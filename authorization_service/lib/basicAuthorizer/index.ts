6938c9e9b7acf6c5941877ec9f294822721541d0
KazikKluz <kazik.kluz@gmail.com>
2025-03-23 01:11:49 +0000

    feat: basicAuthorizer

--- authorization_service/lib/basicAuthorizer/index.ts
+++ authorization_service/lib/basicAuthorizer/index.ts
@@ -1,9 +1,14 @@
+import {
+  APIGatewayTokenAuthorizerEvent,
+  APIGatewayAuthorizerResult,
+} from 'aws-lambda';
+
 import basicAuthorizer from './controller';
 
-exports.handler = async (event: S3Event): Promise<APIGatewayResponse> => {
-  console.log('importFileParser request', {
-    records: event.Records,
-  });
+exports.handler = async (
+  event: APIGatewayTokenAuthorizerEvent
+): Promise<APIGatewayAuthorizerResult> => {
+  console.log('basicAuthorizer event: ', JSON.stringify(event));
 
-  return await basicAuthorizer();
+  return await basicAuthorizer(event.authorizationToken, event.methodArn);
 };
